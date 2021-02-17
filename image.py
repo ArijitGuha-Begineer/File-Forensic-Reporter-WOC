bf9bc3426365dfa09e6f3a398357555c9a6751a7
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-02-17 23:34:25 +0530

    Update image.py

--- image.py
+++ image.py
@@ -4,7 +4,7 @@ def strings(loc):   #calling strings command for PNG and JPG files
    st=subprocess.run(['strings',loc],capture_output=True,text=True)
    print(st.stdout)
 
-strings('/home/arijit/Downloads/rocks.jpg')
+
 
 
 
