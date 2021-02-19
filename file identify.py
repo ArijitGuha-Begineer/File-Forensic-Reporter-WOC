96ae9d204a06f51595643f04520e753b98b8c0d5
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-02-19 06:23:36 -0600

    Update file identify.py

--- file identify.py
+++ file identify.py
@@ -1,6 +1,17 @@
 import subprocess
-
+import re
+import os
 
 def fileidentification(loc):
-	command=subprocess.run(['file',loc],capture_output=True,text=True)
-	print(command.stdout)
+ command=subprocess.run(['file',loc],capture_output=True,text=True)
+ text=re.findall(r"\w+",command.stdout)
+
+ 
+if('PNG' in text):
+	os.system('image.py')
+
+elif('JPEG' in text):
+	os.system('image.py')
+
+else:
+	print('file not recognisable')
