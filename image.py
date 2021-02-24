41c7544c054b3074d9a7634b30a1977e6ce6fe75
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-02-24 20:16:56 +0530

    Update image.py

--- image.py
+++ image.py
@@ -4,6 +4,23 @@ def strings(loc):   #calling strings command for PNG and JPG files
    st=subprocess.run(['strings',loc],capture_output=True,text=True)
    print(st.stdout)
 
+def exiftool(loc):  #calling exiftool for PNG and JPG files
+   ex=subprocess.run(['exiftool',loc],capture_output=True,text=True)
+   print(ex.stdout)
+
+def zsteg(loc):     #calling zsteg for PNG files
+   zs=subprocess.run(['zsteg',loc],capture_output=True,text=True)
+   print(zs.stdout)
+
+loc='Enter your location of file'
+def main(loc):
+	strings(loc)
+	exiftool(loc)
+	zsteg(loc)
+
+if __name__ == '__main__':
+	main(loc)
+
 
 
 
