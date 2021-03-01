5b0e1fc6e44244714bfd83a7d4d39ac54a43a7d9
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-03-01 20:50:45 +0530

    Update txt.py

--- txt.py
+++ txt.py
@@ -9,10 +9,16 @@ def strings(loc):
 	st=subprocess.run(['strings',loc],capture_output=True,text=True)
 	print(st.stdout)
 
+def binwalk(loc):
+	bn=subprocess.run(['binwalk',loc],capture_output=True,text=True)
+	print(bn.stdout)
+
+
 
 def main(loc):
   cat(loc)
   strings(loc)
+  binwalk()
 
 
 if __name__ == '__main__':
