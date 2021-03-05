b542d92c5e19db1f9be61aac816ef143f2cb40a3
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-03-05 20:13:20 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,35 +1,54 @@
 import subprocess
+import re
+
+loc='/home/arijit/Downloads/rocks.jpg'
 
 def strings(loc):   #calling strings command for PNG and JPG files
-   st=subprocess.run(['strings',loc],capture_output=True,text=True)
-   print(st.stdout)
+   st=subprocess.Popen(['strings',loc],stdout=subprocess.PIPE)
+   output=st.communicate()
+   print(output)
 
 def exiftool(loc):  #calling exiftool for PNG and JPG files
-   ex=subprocess.run(['exiftool',loc],capture_output=True,text=True)
-   print(ex.stdout)
+   st=subprocess.Popen(['exiftool',loc],stdout=subprocess.PIPE)
+   
+   output=st.communicate()
+   print(output)
+   
 
 def zsteg(loc):     #calling zsteg for PNG files
-   zs=subprocess.run(['zsteg',loc],capture_output=True,text=True)
-   print(zs.stdout)
+   st=subprocess.Popen(['zsteg',loc],stdout=subprocess.PIPE)
+   output=st.communicate()
+   print(output)
 
-def binwalk(loc):
-	bn=subprocess.run(['binwalk -e',loc],shell=True,capture_output=True,text=True)
-	print(bn.stdout)
+def binwalk(loc):   #reveals metadata stored in the image
+   st=subprocess.Popen(['binwalk',loc],stdout=subprocess.PIPE)
+   output=st.communicate()
+   print(output)
+
 
 def stegseek(loc):
-	sk=subprocess.run(['stegseek',loc,'enter your wordlist.txt'],capture_output=True,text=True)
-    print(sk.stdout)
+   st=subprocess.Popen(['stegseek',loc,'/home/arijit/Downloads/rockyou.txt'],stdout=subprocess.PIPE)
+   output=st.communicate()
+   print(output)
 
-loc=''
+
+def imagemagick(loc):
+   st=subprocess.Popen(['identify','-verbose',loc],stdout=subprocess.PIPE)
+   output=st.communicate()
+   print(output)
+
 def main(loc):
 	strings(loc)
 	exiftool(loc)
 	zsteg(loc)
 	binwalk(loc)
 	stegseek(loc)
+	imagemagick(loc)
+	
 
 if __name__ == '__main__':
 	main(loc)
+
 
 
 
