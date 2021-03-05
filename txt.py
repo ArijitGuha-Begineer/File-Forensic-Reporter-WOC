cd0493d227ae2444eb8e0c2a2efa65db813977fa
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-03-05 20:47:35 +0530

    Update txt.py

--- txt.py
+++ txt.py
@@ -1,25 +1,28 @@
 import subprocess
 
-def cat(loc):
-  ct=subprocess.run(['cat',loc],capture_output=True,text=True)
-  print(ct.stdout)
+loc='/home/arijit/Downloads/substitution2.txt'
 
+def cat(loc):
+  st=subprocess.Popen(['cat',loc],stdout=subprocess.PIPE)
+  output=st.communicate()
+  print(output)
 
 def strings(loc):
-	st=subprocess.run(['strings',loc],capture_output=True,text=True)
-	print(st.stdout)
+  import image
+  image.strings(loc)
 
 def binwalk(loc):
-	bn=subprocess.run(['binwalk',loc],capture_output=True,text=True)
-	print(bn.stdout)
+  import image
+  image.binwalk(loc)
 
-
+def xxd(loc):
+  import image
+  image.xxd(loc)
 
 def main(loc):
-  cat(loc)
   strings(loc)
-  binwalk()
-
-
+  strings(loc)
+  binwalk(loc)
+  xxd(loc)
 if __name__ == '__main__':
   main(loc)
