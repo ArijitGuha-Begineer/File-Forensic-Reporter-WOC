e9e7b8a237b02afebad77291a764ddb64d37a1ae
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-03-08 17:30:10 +0530

    Update zip.py
    adding zipfile module for zip file analysis

--- zip.py
+++ zip.py
@@ -2,12 +2,12 @@ import zipfile
 
 loc='/home/arijit/Downloads/task.zip'
 
-def main(loc):
-	with zipfile.ZipFile(loc,'r') as my_zip:
-	print(my_zip.namelist())
 
-if __name__ == '__main__':
-	main(loc)
+with zipfile.ZipFile(loc,'r') as my_zip:
+    print(my_zip.namelist())
+
+
+
 
 	
 
