c7449028b5748a4ed96055aadffbc7fcc523f8c0
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-03-21 17:06:29 +0530

    Update zip.py

--- zip.py
+++ zip.py
@@ -1,11 +1,19 @@
 import zipfile
+import subprocess
 
-loc='/home/arijit/Downloads/task.zip'
+loc='/home/arijit/Downloads/TRUMP.zip'
 
 
-with zipfile.ZipFile(loc,'r') as my_zip:
-    print(my_zip.namelist())
 
+
+try:
+	    with zipfile.ZipFile(loc,'r') as my_zip:
+	    	my_zip.extractall(loc)
+
+except:
+	   data=subprocess.Popen(['fcrackzip','-u','-D','-p','/home/arijit/Downloads/rockyou.txt',loc],stdout=subprocess.PIPE)
+	   output=data.communicate()[0]
+	   print(output)
 
 
 
