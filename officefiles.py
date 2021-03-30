70085b0ace4da7a5558211e1691e8f366817d0e8
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-03-30 20:45:29 +0530

    Update officefiles.py

--- officefiles.py
+++ officefiles.py
@@ -1,14 +1,33 @@
 import subprocess
+import re
 
 loc="/home/arijit/Downloads/weird.docm"
 
 def olevba(loc):
     st=subprocess.Popen(['olevba','-c',loc],stdout=subprocess.PIPE)
     output=st.communicate()[0]
-    print(output)
+    with open("Report.md","a")as f:
+        f.write("output")
+        f.close()
+    with open("Report.md","a+b")as f:
+        f.write(output)
+        f.close()
+
+def mraptor(loc):
+    st=subprocess.Popen(['mraptor',loc],stdout=subprocess.PIPE)
+    output=st.communicate()[0]
+    with open("Report.md","a")as f:
+        f.write("output")
+        f.close()
+    with open("Report.md","a+b")as f:
+        f.write(output)
+        f.close()
+    
+        
 
 def main(loc):
     olevba(loc)
+    mraptor(loc)
 
 if __name__ == '__main__':
 	main(loc)
