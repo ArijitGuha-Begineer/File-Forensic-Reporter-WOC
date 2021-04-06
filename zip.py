3b4c8f914c32ae34e786b114e47b18ba15a4a39e
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-04-06 21:19:55 +0530

    Update zip.py

--- zip.py
+++ zip.py
@@ -13,7 +13,9 @@ try:
 except:
 	   data=subprocess.Popen(['fcrackzip','-u','-D','-p','/home/arijit/Downloads/rockyou.txt',loc],stdout=subprocess.PIPE)
 	   output=data.communicate()[0]
-	   print(output)
+	   with open("Report.md","a+b") as f:
+		   f.write(output)
+		   f.close()
 
 
 
