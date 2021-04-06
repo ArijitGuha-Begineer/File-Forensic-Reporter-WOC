c22bdc77027a5b3f455927d4b8478afea49dc2e7
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-04-06 21:21:37 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -127,3 +127,70 @@ def foremost(loc):
    with open("/home/arijit/Downloads/Report.md","a+b")as f:
       f.write(output)
       f.close()
+
+def wavsteg(loc):
+  st=subprocess.Popen(['stegolsb','wavesteg','-r','-i',loc,'-o','output.txt','-b','10000'],stdout=subprocess.PIPE)
+  output=st.communicate()[0]
+  with open('output.txt','rb')as f1:
+	  text=f1.read()
+	  f1.close()
+  with open("/home/arijit/Downloads/Report.md","a")as f:
+      f.write("\n\n*********output*******\n\n")
+      f.close()
+  with open("/home/arijit/Downloads/Report.md","a+b")as f:
+      f.write(output)
+      f.close()
+
+
+def pdfid(loc):  #finding any hidden hash within the pdf file
+  text=subprocess.Popen([f'pdfid {loc} | grep -v -E "xref|trailer|/AA|/OpenAction|/Acroform|/XFA|/RichMedia|startxref" '],shell=True,stdout=subprocess.PIPE)
+  output=text.communicate()[0]
+  with open("/home/arijit/Downloads/Report.md","a")as f:
+      f.write("\n\n*********output*******\n\n")
+      f.close()
+  with open("/home/arijit/Downloads/Report.md","a+b")as f:
+      f.write(output)
+      f.close()
+
+def olevba(loc):
+    st=subprocess.Popen(['olevba','-c',loc],stdout=subprocess.PIPE)
+    output=st.communicate()[0]
+    with open("/home/arijit/Downloads/Report.md","a")as f:
+        f.write("output")
+        f.close()
+    with open("/home/arijit/Downloads/Report.md","a+b")as f:
+        f.write(output)
+        f.close()
+
+def mraptor(loc):
+    st=subprocess.run(['mraptor',loc],capture_output=True,text=True)
+    ans=re.search('SUSPICIOUS',st.stdout)
+    if ans==None:
+        with open("/home/arijit/Downloads/Report.md","a")as f:
+         f.write("\n\n******NO SUSPICIOUS FILES*******\n\n")
+         f.close()
+    else:
+        with open("/home/arijit/Downloads/Report.md","a")as f:
+         f.write("\n\n******SUSPICIOUS FILES PRESENT*******\n\n")
+         f.close()
+
+
+def pyxswf(loc):
+    st=subprocess.Popen(['pyxswf','-x',loc],stdout=subprocess.PIPE)
+    output=st.communicate()[0]
+    with open("/home/arijit/Downloads/Report.md","a")as f:
+        f.write("\n\n******output*******\n\n")
+        f.close()
+    with open("/home/arijit/Downloads/Report.md","a+b")as f:
+        f.write(output)
+        f.close()
+
+def msodde(loc):
+    st=subprocess.Popen(['msodde','-a',loc],stdout=subprocess.PIPE)
+    output=st.communicate()[0]
+    with open("/home/arijit/Downloads/Report.md","a")as f:
+        f.write("\n\n******output*******\n\n")
+        f.close()
+    with open("/home/arijit/Downloads/Report.md","a+b")as f:
+        f.write(output)
+        f.close()
