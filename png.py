af3a6fdd8ceaf18c76d610dca0c9a8fcb6492f1e
ArijitGuha-Begineer <78166271+ArijitGuha-Begineer@users.noreply.github.com>
2021-04-08 10:08:44 +0530

    Add files via upload

---
+++ png.py
@@ -0,0 +1,16 @@
+import subprocess
+import modules
+
+
+def main(loc):
+   modules.cat(loc)
+   modules.strings(loc)
+   modules.exiftool(loc)
+   modules.binwalk(loc)
+   modules.zsteg(loc)
+   modules.xxd(loc)
+   modules.outguess(loc)
+   modules.foremost(loc)
+   modules.stegseek(loc)
+   modules.imagemagick(loc)
+   modules.stegsnow(loc)
